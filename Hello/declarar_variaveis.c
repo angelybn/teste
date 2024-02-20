7cedaf4c669821d7398bcfb168bb7c2cdc4454ab
angelybn <59122409+angelybn@users.noreply.github.com>
2024-02-20 10:51:17 +0000

    commit

---
+++ Hello/declarar_variaveis.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+    int x;
+    x = 10;
+    printf();
+}
+
+//tipos de variaveis
+/*
+float = numeros reais (1.5)
+double = numeros rais, mas ocupa o dobro do espaço
+*/
---
+++ Hello/hello_world.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+//declaracao de variavel
+// tipo e nome
+int main(void) {
+    int x;
+    x = 1;
+    //marcador para separar oq é X texto e oq é X variável
+    printf("x = %d", x);
+}
